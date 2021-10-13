19c8d37ed9f8736b1b9e0c57d5937767b8e10c39
bethuel <bethuel.pg@gmail.com>
2021-10-13 15:54:10 -0300

    13/10 - 16h

---
+++ scrollsuave.js
@@ -0,0 +1,53 @@
+import ('./scrollsuave');
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
+});
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute("href");
+  const to = document.querySelector(id).offsetTop;
+
+/*   window.scroll({
+    top: to,
+    behavior: "smooth",
+  }); */
+
+  smoothScrollTo(0, to);
+}
+
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int} endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+ function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+  
+    duration = typeof duration !== 'undefined' ? duration : 1000;
+  
+    // Easing function
+    const easeInOutQuart = (time, from, distance, duration) => {
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    };
+  
+    const timer = setInterval(() => {
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60); // 60 fps
+  };
